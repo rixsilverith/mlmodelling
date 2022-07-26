a50c6bb7f258b567c346ff40275efd8a5fd6bf9d
Rix Silverith <71402816+rixsilverith@users.noreply.github.com>
2022-07-26 20:16:37 +0200

    Added some examples

---
+++ examples/knn_classifier.py
@@ -0,0 +1,23 @@
+"""K-nearest neighbors classifier example. """
+
+from sklearn.datasets import make_blobs
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+import mlmodels
+
+def main():
+    X, y = make_blobs(n_samples = 300, centers = 2, n_features = 2, cluster_std = 6, random_state = 11)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42)
+
+    print("X_train:", X_train.shape, "y_train:", y_train.shape)
+    print("X_test:", X_test.shape, "y_test:", y_test.shape)
+
+    model = mlmodels.neighbors.KNeighborsClassifier(k_neighbors = 151).fit(X_train, y_train)
+    model.summary()
+
+    y_pred = model.predict(X_test)
+    print(f'acc score: {float(np.mean(y_pred == y_test)):.2f}')
+
+if __name__ == '__main__':
+    main()
---
+++ examples/random_forest_classifier.py
@@ -0,0 +1,29 @@
+"""Random forest classifier example. """
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+
+import mlmodels
+
+def main():
+    X, y = datasets.make_blobs(n_samples=1000, centers=2, n_features=2, cluster_std=3, random_state=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+    print("X_train:", X_train.shape, "y_train:", y_train.shape)
+    print("X_test:", X_test.shape, "y_test:", y_test.shape)
+    
+    model = mlmodels.decision_trees.RandomForestClassifier(criterion = 'entropy', 
+        n_estimators = 250, min_samples_split = 2, max_depth = 3)
+
+    model.fit(X_train, y_train)
+    model.summary()
+
+    y_pred = model.predict(X_test)
+    print(f'acc score: {np.mean(y_pred == y_test):.2f}')
+
+if __name__ == "__main__":
+    main()
+
